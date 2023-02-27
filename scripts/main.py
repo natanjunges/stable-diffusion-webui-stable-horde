d6b64b1ab5f9653c3a43447fe8bdabd645be0c26
Donald Nixon <donald@nodero.com>
2023-02-27 15:20:28 +1300

    fix fix

--- scripts/main.py
+++ scripts/main.py
@@ -178,7 +178,7 @@ class Main(SettingsManager, scripts.Script):
         try:
             for k, v in p.override_settings.items():
                 setattr(shared.opts, k, v)
-            print (f"Value of p = {shared.opts}")
+            print (f"Value of p = {vars(shared.opts)}")
             p.extra_generation_params = {
                 "Model": model,
                 "NSFW": nsfw,
